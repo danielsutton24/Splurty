1c4aad5a9449028d71d80ed5a20a9e9990963c4f
Daniel <sutton.daniel.ds@gmail.com>
2019-08-18 20:14:17 +0000

    new schema

--- db/migrate/20190811210233_alter_quotes_add_source.rb
+++
@@ -1,5 +0,0 @@
-class AlterQuotesAddSource < ActiveRecord::Migration[5.2]
-  def change
-    add_column :quotes, :source, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_11_210233) do
+ActiveRecord::Schema.define(version: 2019_08_18_201131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_08_11_210233) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "source"
   end
 
 end
